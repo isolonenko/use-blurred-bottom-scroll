e5dcb1e5e3a9b87b740a1bdcb36d759911144000
isolonenko <103060679+isolonenko@users.noreply.github.com>
2022-11-28 15:22:12 +0300

    remove react dom from deps

--- src/__tests__/blurred.test.ts
+++ src/__tests__/blurred.test.ts
@@ -1,8 +1,10 @@
 import { useBlurredBottomScroll } from '../useBlurredBottomScroll';
 import { renderHook } from '@testing-library/react-hooks';
+import { act } from 'react-test-renderer';
 
 test('Rendered', () => {
   const { result } = renderHook(() => useBlurredBottomScroll());
+  const { blurredElement } = result.current;
 
-  expect(result.current).toBeTruthy();
+  expect(blurredElement).toBeTruthy();
 });
